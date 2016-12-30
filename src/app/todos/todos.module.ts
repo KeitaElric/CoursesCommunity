624166b2cdbf7af668c52a6b9d6e9d488007ba23
Thong <hvqthong.pns@gmail.com>
2016-12-30 15:19:41 +0700

    Create menu

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -11,40 +11,6 @@ import { TodosListComponent } from './components/todos-list/todos-list.component
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
-import {MdCardModule} from '@angular2-material/card';
-import {MdIconModule} from '@angular2-material/icon';
-import {MdButtonModule} from '@angular2-material/button';
-import {MdIconRegistry} from '@angular2-material/icon';
-import {MdToolbarModule} from '@angular2-material/toolbar';
-import {MdInputModule} from '@angular2-material/input';
-import {MdCheckboxModule} from '@angular2-material/checkbox';
-import {MdListModule} from '@angular2-material/list';
-
-const MATERIAL_MODULES = [
-MdButtonModule,
-//MdButtonToggleModule,
-MdCardModule,
-MdCheckboxModule,
-//MdGridListModule,
-MdIconModule,
-MdInputModule,
-MdListModule,
-//MdMenuModule,
-//MdProgressBarModule,
-//MdProgressCircleModule,
-//MdRadioModule,
-//MdRippleModule,
-//MdSidenavModule,
-//MdSliderModule,
-//MdSlideToggleModule,
-//MdTabsModule,
-MdToolbarModule,
-//MdTooltipModule,
-//OverlayModule,
-//PortalModule,
-//RtlModule,
-];
-
 export class AppModule { }
 @NgModule({
     imports: [
@@ -52,8 +18,7 @@ export class AppModule { }
         FormsModule,
         HttpModule,
         TodosRoutingModule,
-        MATERIAL_MODULES
-        // MaterialModule.forRoot()
+        MaterialModule.forRoot()
     ],
     exports: [],
     declarations: [
